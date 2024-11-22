7c20f29687c6abd0ec6f296b6d9b1108cfcb9dbd
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-11-22 15:43:43 -0500

    Understanding Side Effects in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log("APP COMPONENT");
+    const [resourceType, setResourceType] = useState("HOME");
+
+
+
+    useEffect(() => {
+        console.log("RESOURCE TYPE", resourceType);
+    }, [resourceType])
+
+    return (
+        <div className="App">
+            <button onClick={() => setResourceType("HOME")}>HOME</button>
+            <button onClick={() => setResourceType("ABOUT")}>ABOUT</button>
+            <button onClick={() => setResourceType("CONTACT")}>CONTACT</button>
+            <h3>{resourceType}</h3>
+        </div>
+    );
 }
 
 export default App;
